23ceab397f13311aa00e71d120d5535b05fa93b8
nuwan-github <nuwangithub@gmail.com>
2019-03-20 23:24:30 +0530

    implement dynamic side bar skeleton

--- emp-man-web-app/src/app/components/shared/sidebar/sidebar/sidebar.component.ts
+++ emp-man-web-app/src/app/components/shared/sidebar/sidebar/sidebar.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SidebarService } from './sidebar.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.scss'],
+  animations: [
+    trigger('slide', [
+      state('up', style({ height: 0 })),
+      state('down', style({ height: '*' })),
+      transition('up <=> down', animate(200))
+    ])
+  ]
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  menus = [];
+
+  constructor(public sidebarservice : SidebarService) {
+    this.menus = sidebarservice.menus;
+   }
 
   ngOnInit() {
+  }
+
+  getSideBarState() {
+    return this.sidebarservice.getSidebarState();
+  }
+
+  hasBackgroundImage() {
+    return this.sidebarservice.hasBackgroundImage;
+  }
+
+  getState(currentMenu) {
+
+    if (currentMenu.active) {
+      return 'down';
+    } else {
+      return 'up';
+    }
+  }
+
+  toggle(currentMenu) {
+    if (currentMenu.type === 'dropdown') {
+      this.menus.forEach(element => {
+        if (element === currentMenu) {
+          currentMenu.active = !currentMenu.active;
+        } else {
+          element.active = false;
+        }
+      });
+    }
   }
 
 }
